04d73db86013c4662a223ebd87f5d5e7299d722f
Raniel e Janaina <ranielcorreia1997@gmail.com>
2017-12-15 16:31:06 -0300

    criacao dos metodos construtores e Geters e Seters e o ToString

--- JogosMortais/src/br/edu/ifal/Professor.java
+++ JogosMortais/src/br/edu/ifal/Professor.java
@@ -5,4 +5,35 @@ public class Professor {
 	private String formacao;
 	private String cpf;
 	
+	public Professor() {
+		super();
+		nome = "";
+		formacao = "";
+		cpf = "";
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFormacao() {
+		return formacao;
+	}
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public String toString() {
+		return "Professor [nome=" + nome + ", formacao=" + formacao + ", cpf=" + cpf + "]";
+	}
+
 }
