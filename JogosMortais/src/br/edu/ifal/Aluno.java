597f9db142fb77dcb24a76fa8b759155b107d2df
wanessaramos <ranielcorreia1997@gmail.com>
2017-12-15 16:25:34 -0300

    Criação Classe Aluno

---
+++ JogosMortais/src/br/edu/ifal/Aluno.java
@@ -0,0 +1,25 @@
+package br.edu.ifal;
+
+public class Aluno {
+	private String nome;
+	private String matricula;
+	
+	public Aluno() {
+		super();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	
+}
