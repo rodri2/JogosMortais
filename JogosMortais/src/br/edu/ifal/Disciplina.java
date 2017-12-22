a9aea27c331869ce655d708b65685ac899e46ce3
Raniel Silva <ranielcorreia1997@gmail.com>
2017-12-22 14:15:48 -0300

    Criação dos métodos getters e setters e do ToString

--- JogosMortais/src/br/edu/ifal/Disciplina.java
+++ JogosMortais/src/br/edu/ifal/Disciplina.java
@@ -15,5 +15,40 @@ public class Disciplina {
 		professor = new Professor();
 		aluno = new ArrayList<>();
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (nome != null) {
+			this.nome = nome;
+		}
+	}
+
+	public Professor getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(Professor professor) {
+		if (professor != null) {
+			this.professor = professor;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Disciplina [nome=" + nome + ", professor=" + professor + ", aluno=" + aluno + "]";
+	}
+
+	public List<Aluno> getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(List<Aluno> aluno) {
+		if (aluno != null) {
+			this.aluno = aluno;
+		}
+	}
 	
 }
