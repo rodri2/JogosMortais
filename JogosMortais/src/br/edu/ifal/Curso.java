b71c3121c5e720d5534f8a802f5164e36ba38159
jana e rodri2 <janainarodriguesdemiranda@gmail.com>
2017-12-22 14:29:45 -0300

    criação dos metodos getters e setters

--- JogosMortais/src/br/edu/ifal/Curso.java
+++ JogosMortais/src/br/edu/ifal/Curso.java
@@ -1,5 +1,6 @@
 package br.edu.ifal;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Curso {
@@ -10,15 +11,45 @@ public class Curso {
 	public Curso() {
 		super();
 		nome = "";
-		aluno = "";
-		disciplina = ""
+		aluno = new ArrayList<>();
+		disciplina = new ArrayList<>();
 	}
 
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (nome != null) {
+			this.nome = nome;
+		}
+	}
+
+	public List<Aluno> getAluno() {
+		return aluno;
+	}
+
+	public void setAluno(List<Aluno> aluno) {
+		if (aluno != null) {
+			this.aluno = aluno;
+		}
+	}
+
+	public List<Disciplina> getDisciplina() {
+		return disciplina;
+	}
+
+	public void setDisciplina(List<Disciplina> disciplina) {
+		if (disciplina != null) {
+			this.disciplina = disciplina;
+		}
+	}
+	
 	@Override
 	public String toString() {
 		return "Curso [aluno=" + aluno + "]";
 	}
-	
-	
 	
 }
